2f5ab226a989912abf998113c846ccd13bcffd0c
Vladimir Pouzanov <farcaller@gmail.com>
2012-01-28 17:34:00 +0200

    Added a target for tests with kiwi

---
+++ InjectiveTests/InjectiveTests.h
@@ -0,0 +1,13 @@
+//
+//  InjectiveTests.h
+//  InjectiveTests
+//
+//  Created by Vladimir Pouzanov on 1/28/12.
+//  Copyright (c) 2012 Ciklum ApS. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface InjectiveTests : SenTestCase
+
+@end
